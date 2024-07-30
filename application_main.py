70ace794a929eeddd7c81d66e3817723adc01b9b
unknown <navjotsharma0508@gmail.com>
2024-07-30 13:06:43 -0700

    reverted the changes

--- application_main.py
+++ application_main.py
@@ -13,7 +13,7 @@ if __name__=='__main__':
     spark=Utils.get_spark_session(job_run_env)
     logger=Log4j(spark)
     logger.info("spark session created")
-    #print("created spark Session")
+    print("created spark Session")
     orders_df=DataReader.read_orders(spark,job_run_env)
     orders_filter=DataManipulation.filter_closed_orders(orders_df)
 
@@ -23,6 +23,6 @@ if __name__=='__main__':
     aggregated_results=DataManipulation.count_order(joined_df)
 
     aggregated_results.show()
-    #print("main end")
+    print("main end")
 
-    logger.info("this is the end of main")
+    logger.warn("this is the end of main")
